a9454c9529097736b7931d4dfdb3338cf4450a0b
John Eskandar <johng.eskandar@gmail.com>
2023-02-06 00:50:36 +0200

    add translation and formik

---
+++ src/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationEn from './locales/en/translation.json';
+import translationAr from './locales/ar/translation.json';
+
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+const resources = {
+  en: {
+    translation: translationEn,
+  },
+  ar: {
+    translation: translationAr,
+  },
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    fallbackLng: 'ar',
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+export default i18n;
