86f85fc350dab60d12918e12449073710a035a4f
John Eskandar <johng.eskandar@gmail.com>
2023-02-06 02:21:36 +0200

    add helmet, language detector and fontawesome

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEn from './locales/en/translation.json';
 import translationAr from './locales/ar/translation.json';
 
@@ -17,10 +18,11 @@ const resources = {
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
     resources,
-    fallbackLng: 'ar',
-
+    lng: 'ar',
+    fallbackLng: ['ar', 'en'],
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
