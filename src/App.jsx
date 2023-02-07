f0630c30dcc639253fb30b3069f5fbc5469f4f61
John Eskandar <johng.eskandar@gmail.com>
2023-02-07 21:18:32 +0200

    level up language helmet to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
 import { useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { useTranslation } from 'react-i18next';
 import Homepage from './components/Homepage';
 import Game from './components/Game';
 
 const App = () => {
+  const { i18n } = useTranslation();
   const [gameStarted, setGameStarted] = useState(false);
   return (
-    <div className="app">
-      {gameStarted ? <Game /> : <Homepage setGameStarted={setGameStarted} />}
-    </div>
+    <>
+      <Helmet htmlAttributes={{ lang: i18n.language, dir: i18n.dir(i18n.language) }} />
+      <div className="app">
+        {gameStarted ? <Game /> : <Homepage setGameStarted={setGameStarted} />}
+      </div>
+    </>
   );
 };
 
