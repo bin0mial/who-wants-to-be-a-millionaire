696bea92993ac6e9c8c1c409b63cf8b9be303e6e
John Eskandar <johng.eskandar@gmail.com>
2023-03-31 00:51:34 +0200

    fix timer freeze

--- src/components/Questions/Timer/Timer.jsx
+++ src/components/Questions/Timer/Timer.jsx
@@ -1,19 +1,16 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Timer.css';
 import PropTypes from 'prop-types';
 
 const Timer = ({ isReady, timeInSeconds, onTimeout }) => {
   const [timeCountDown, setTimeCountDown] = useState(timeInSeconds);
-  const initialWaitFinish = useRef(false);
 
   useEffect(() => {
     setTimeout(() => {
-      if (initialWaitFinish.current && timeCountDown > 0 && isReady) {
+      if (timeCountDown > 0 && isReady) {
         setTimeCountDown((oldValue) => oldValue - 1);
       } else if (timeCountDown <= 0) {
         onTimeout();
-      } else {
-        initialWaitFinish.current = true;
       }
     }, 1000);
   }, [timeCountDown, isReady]);
