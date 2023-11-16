1c4e232581fe40c6ee8731ab65a30181138574ef
John Eskandar <54377774+bin0mial@users.noreply.github.com>
2023-11-17 00:20:40 +0200

    61 share meta data for analytics (#62)
    * upgrade firebase version
    
    * add share metadata timestamp and questions count
    
    * add track users most visited pages
    
    * add loader until app initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import Homepage from 'components/Homepage';
@@ -7,6 +7,7 @@ import Game from 'components/Game';
 import GameControlContext from 'contexts/GameControlContext';
 import ReactGA from 'react-ga4';
 import initApp from 'helpers/app';
+import AppLoading from './components/Shared/Loadings/AppLoading';
 
 ReactGA.initialize(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID);
 
@@ -16,15 +17,23 @@ const App = () => {
 
   const isInitialized = initApp();
 
+  useEffect(() => {
+    if (isInitialized) {
+      const sharedId = (new URLSearchParams(window.location.search)).get('shareId');
+      const title = sharedId ? `Custom ${sharedId}` : 'Homepage';
+      ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search, title });
+    }
+  }, [window.location, isInitialized]);
+
   return (
     <>
       <Helmet htmlAttributes={{ lang: i18n.language, dir: i18n.dir(i18n.language) }} />
 
-      {isInitialized && (
-      <div className="app">
-        {gameStarted ? <Game /> : <Homepage />}
-      </div>
-      )}
+      {isInitialized ? (
+        <div className="app">
+          {gameStarted ? <Game /> : <Homepage />}
+        </div>
+      ) : <AppLoading />}
     </>
   );
 };
